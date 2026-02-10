1d7695535e9a141825d74143d82f4bdb16e1a184
Oliver Díaz <oliverdiaz130602@gmail.com>
2026-02-10 12:43:37 -0600

    creacion de las paginas de opiniones

--- assets/js/search.js
+++ assets/js/search.js
@@ -17,6 +17,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 searchPath = '../../search/index.html';
             } else if (path.includes('/pages/category/')) {
                 searchPath = '../search/index.html';
+            } else if (path.includes('/pages/opiniones/')) {
+                searchPath = '../search/index.html';
             } else if (path.includes('/pages/legal/')) {
                 searchPath = '../search/index.html';
             } else if (path.includes('/pages/search/')) {
